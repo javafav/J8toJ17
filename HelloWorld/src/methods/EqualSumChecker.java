027008fb09b7129ff6cb1f4f37cd016030b99276
sanykhan <usmanshj3@gmail.com>
2024-09-14 17:42:12 +0500

    Practicing Java 17 features: some more  method challengs covred

---
+++ HelloWorld/src/methods/EqualSumChecker.java
@@ -0,0 +1,8 @@
+package methods;
+
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int a, int b, int c) {
+        return (a + b) == c;
+    }
+}
---
+++ HelloWorld/src/methods/TeenNumberChecker.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class TeenNumberChecker {
+
+    public static boolean hasTeen(int a, int b, int c) {
+        return isTeen(a) || isTeen(b) || isTeen(c);
+    }
+
+    public static boolean isTeen(int number) {
+        return number > 12 && number < 20;
+    }
+}
