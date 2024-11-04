e1962686578f829a870d2fba058b81ba5169fc99
sanykhan <usmanshj3@gmail.com>
2024-11-04 20:35:58 +0500

    mplemented examples of the reduce method in Java Streams for aggregation and summarization

--- HelloWorld/src/oop/streams/student/MainCollect.java
+++ HelloWorld/src/oop/streams/student/MainCollect.java
@@ -1,7 +1,6 @@
 package oop.streams.student;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,10 +13,52 @@ public class MainCollect {
                 .limit(1000)
                 .collect(Collectors.toList());
 
-        Set<Student> ausStudent =students.stream()
+        Set<Student> australianStudents = students.stream()
                 .filter(std -> std.getCountryCode().equals("AU"))
                 .collect(Collectors.toSet());
 
-        System.out.println(" # of Aus Student " + ausStudent.size());
+        System.out.println(" # of Aus Student " + australianStudents.size());
+
+
+        Set<Student> underThirty = students.stream()
+                // .filter(std -> std.getCountryCode().equals("AU"))
+                .filter(std -> std.getAgeEnrolled() < 30)
+                .collect(Collectors.toSet());
+
+        System.out.println(" # of Aus  Student of Age < 30 =  " + underThirty.size());
+
+        Set<Student> youngAussies1 = new TreeSet<>(Comparator.comparing(
+                Student::getStudentId));
+
+        youngAussies1.addAll(australianStudents);
+        youngAussies1.retainAll(underThirty);
+        youngAussies1.forEach(s -> System.out.print(s.getStudentId() + " "));
+        System.out.println();
+
+        var youngAussies2 = students.stream()
+                // .filter(std -> std.getCountryCode().equals("AU"))
+                .filter(std -> std.getAgeEnrolled() < 30)
+                .filter(std -> std.getCountryCode().equals("AU"))
+                .collect(() -> new TreeSet(Comparator.comparing(
+                        Student::getStudentId)), TreeSet::add, TreeSet::addAll);
+
+        // youngAussies2.forEach( (s) -> System.out.print(s.getStudentId() + " "));
+
+//youngAussies2.forEach((Student) s -> System.out.println(s.getStudentId() + " "));
+
+        String countryList = students.stream()
+                .map(Student::getCountryCode)
+                .distinct()
+                .sorted()
+                .reduce("", (countryList1,nextCountry) -> countryList1 + " " + nextCountry );
+        //"" + Au
+        //Au  + US
+        System.out.println(countryList);
+
+        List<Integer> numbers = Arrays.asList(5, 10, 15);
+        int sum = numbers.stream()
+                .reduce(0, (total, num) -> total + num);
+        System.out.println("Sum: " + sum);  // Output: Sum: 30
+
     }
 }
---
+++ HelloWorld/src/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        String a ="abc";
+        String b ="abc";
+        String x = a.concat(b);
+        System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
