70bce812364c2bded6a5f72126b6ba207708926f
sanykhan <usmanshj3@gmail.com>
2024-11-05 19:41:27 +0500

    Implemented examples of using Optional with Streams for better null handling and data processing

--- HelloWorld/src/test.java
+++ HelloWorld/src/test.java
@@ -1,12 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class test {
     public static void main(String[] args) {
-        String a ="abc";
-        String b ="abc";
-        String x = a.concat(b);
-        System.out.println(x);
+        StringBuilder a =  new StringBuilder("abc");
+        String b = "abc";
+        a.append(b);
+        System.out.println(a);
+
+//        ['foo@a.com', 'bar@a.com', baz@b.com', 'qux@d.com'],
+//
+//        urls=['www.a.com', 'www.b.com', 'www.c.com']
+
+        String[] keyURLs = {"www.a.com", "www.b.com", "www.c.com"};
+        String[] valueEmails ={"foo@a.com", "bar@a.com", "baz@b.com", "qux@d.com"};
+
+        Map<String, String> dictionary = new HashMap<>();
     }
 
-    public static void main(String[] args) {
-        
-    }
+
 }
