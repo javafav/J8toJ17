f795e85c05d4a77a44f398f200e497317878c405
sanykhan <usmanshj3@gmail.com>
2024-11-12 18:41:50 +0500

    continue discussion on Math.random() usage and examples

--- HelloWorld/src/math/MathFunction.java
+++ HelloWorld/src/math/MathFunction.java
@@ -1,5 +1,7 @@
 package math;
 
+import java.util.Random;
+
 public class MathFunction {
 
     public static void main(String[] args) {
@@ -31,6 +33,53 @@ public class MathFunction {
         for (int i = 0; i < 10; i++) {
             System.out.println(Math.random());
         }
+
+
+        System.out.println("---------------------------");
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%1$d = %1$c%n", r.nextInt(65, 91));
+        }
+
+        System.out.println("---------------------------");
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%1$d = %1$c%n",
+                    r.nextInt((int) 'A', (int) 'Z' + 1));
+        }
+
+        System.out.println("---------------------------");
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%1$d%n", r.nextInt(-10, 11));
+        }
+
+        System.out.println("---------------------------");
+        r.ints()
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("---------------------------");
+        r.ints(0, 10)
+                .limit(10)
+                .forEach(System.out::println);
+
+        System.out.println("---------------------------");
+        r.ints(10, 0, 10)
+                .forEach(System.out::println);
+
+        System.out.println("---------------------------");
+        r.ints(10)
+                .forEach(System.out::println);
+
+        long nanoTime = System.nanoTime();
+        Random pseudoRandom = new Random(nanoTime);
+        System.out.println("---------------------------");
+        pseudoRandom.ints(10, 0, 10)
+                .forEach(i -> System.out.print(i + " "));
+
+        Random notReallyRandom = new Random(nanoTime);
+        System.out.println("\n---------------------------");
+        notReallyRandom.ints(10, 0, 10)
+                .forEach(i -> System.out.print(i + " "));
 
     }
 }
