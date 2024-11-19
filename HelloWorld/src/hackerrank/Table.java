e936a035d46ad18038cfc8cb76ae3701912b1abf
sanykhan <usmanshj3@gmail.com>
2024-11-19 22:09:32 +0500

    Some coding excercise

---
+++ HelloWorld/src/hackerrank/Table.java
@@ -0,0 +1,18 @@
+package hackerrank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Table {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(N + " x " + i + " = " +   N*i);
+        }
+        bufferedReader.close();
+    }
+}
