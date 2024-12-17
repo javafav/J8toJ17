362a707a67615e420f1264825923118e8a5e615c
sanykhan <usmanshj3@gmail.com>
2024-12-17 21:20:26 +0500

    Thread and Java Memory Model try to learn

---
+++ HelloWorld/src/threads/MultipleThreads.java
@@ -0,0 +1,53 @@
+package threads;
+
+import java.util.concurrent.TimeUnit;
+
+public class MultipleThreads {
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch(TimeUnit.SECONDS);
+        Thread green = new Thread(stopWatch::countDown, ThreadColor.ANSI_YELLOW.name());  // here the countDown method reference is target for run function
+                                                                                         //  Thread green = new Thread( () -> stopWatch.countDown(), ThreadColor.ANSI_YELLOW.name());
+        Thread purple = new Thread( () -> stopWatch.countDown(7),
+                  ThreadColor.ANSI_PURPLE.name());
+        Thread red = new Thread(stopWatch::countDown, ThreadColor.ANSI_RED.name());  // here the countDown method reference is target for run function
+        green.start();
+        purple.start();
+
+
+        red.start();
+
+
+    }
+}
+
+class StopWatch {
+    private TimeUnit timeUnit;
+    private int i;
+
+    public StopWatch(TimeUnit timeUnit) {
+        this.timeUnit = timeUnit;
+    }
+
+    public void countDown() {
+        countDown(5);
+    }
+
+    public void countDown(int unitCount) {
+        String threadName = Thread.currentThread().getName();
+        ThreadColor threadColor = ThreadColor.ANSI_RESET;
+        try {
+            threadColor = ThreadColor.valueOf(threadName);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Bad Color Name!!!");
+        }
+        String color = threadColor.color();
+        for ( i = unitCount; i > 0; i--) {
+            try {
+                timeUnit.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.printf("%s%s Thread : i = %d%n", color, threadName, i);
+        }
+    }
+}
