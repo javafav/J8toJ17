38a2339c1d706a50a8a467ccaccd984310b0ce4b
sanykhan <usmanshj3@gmail.com>
2024-12-25 21:40:35 +0500

    Thread practise

---
+++ HelloWorld/src/threads/SynchronizationChallenge.java
@@ -0,0 +1,36 @@
+package threads;
+
+import java.util.Random;
+
+record Order(long orderId, String item, int qty) {
+}
+
+public class SynchronizationChallenge {
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+        ShoeWarehouse warehouse = new ShoeWarehouse();
+        Thread producerThread = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                warehouse.receiveOrder(new Order(random.nextInt(1000000, 9999999),
+                        ShoeWarehouse.PRODUCT_LIST[random.nextInt(0, 5)],
+                        random.nextInt(1, 4)));
+
+            }
+        });
+        producerThread.start();
+
+        for (int i = 0; i < 2; i++) {
+            Thread consumerThread = new Thread(() -> {
+                for (int j = 0; j < 5; j++) {
+                    Order item = warehouse.fullfillOrder();
+
+                }
+            });
+            consumerThread.start();
+
+        }
+
+    }
+
+}
