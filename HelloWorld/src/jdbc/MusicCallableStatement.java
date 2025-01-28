aa6fec1cb44d61981fd43c6c7d456753f2370915
sanykhan <usmanshj3@gmail.com>
2025-01-28 21:49:58 +0500

    Practised JDBC stored procedure calls

--- HelloWorld/src/jdbc/MusicCallableStatement.java
+++ HelloWorld/src/jdbc/MusicCallableStatement.java
@@ -1,10 +1,15 @@
 package jdbc;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.*;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static jdbc.PreparedStatementDemo.printRecords;
 
 public class MusicCallableStatement {
 
@@ -33,5 +38,46 @@ public class MusicCallableStatement {
                 System.out.println(key + " : " + value);
             });
         });
+
+
+        var datasource = new MysqlDataSource();
+
+        datasource.setServerName("localhost");
+        datasource.setPort(3306);
+        datasource.setDatabaseName("music");
+
+
+        try (Connection conn = datasource.getConnection(System.getenv("MYSQL_USER"),
+                System.getenv("MYSQL_PASS"))) {
+
+            CallableStatement cs = conn.prepareCall(
+                    "CALL music.addAlbumInOutCounts(?,?,?,?)");
+
+            albums.forEach((artist, albumMap) -> {
+                albumMap.forEach((album, songs) -> {
+                    try{
+                        cs.setString(1, artist);
+                        cs.setString(2, album);
+                        cs.setString(3, songs);
+                        cs.setInt(4,0); // must initialize the INOUT variable otherwise , it's set to NULL and give wrong calculations
+                        cs.registerOutParameter(4, Types.INTEGER);
+                        cs.execute();
+                        System.out.printf("%d songs were added for %s%n", cs.getInt(4), album);
+                    }catch (SQLException e){
+                        System.err.println(e.getErrorCode() + " " + e.getMessage());
+                    }
+                });
+            });
+            String sql = "SELECT * FROM music.albumview WHERE artist_name = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, "Bob Dylan");
+            ResultSet rs = ps.executeQuery();
+            printRecords(rs);
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
