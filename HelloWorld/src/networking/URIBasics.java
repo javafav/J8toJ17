996569f84784065145e4765ffb6d83df68bb6396
sanykhan <usmanshj3@gmail.com>
2025-02-03 20:06:17 +0500

    Practised URL and URI

--- HelloWorld/src/networking/URIBasics.java
+++ HelloWorld/src/networking/URIBasics.java
@@ -1,4 +1,76 @@
 package networking;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class URIBasics {
+    public static void main(String[] args) {
+        URI baseSite = URI.create("https://sastaybrands.com");
+
+        URI sastayBrands  = URI.create(
+                "/c/computer_memory");
+        print(sastayBrands);
+
+        try {
+            URI uri = new URI("" +
+                    "http://user:pw@store.com:5000/products/phone?os=android#samsung");
+            print(uri);
+           URI masterClass = baseSite.resolve(sastayBrands);
+            URL url = masterClass.toURL();
+            System.out.println(url);
+            print(url );
+        } catch (URISyntaxException | MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void  print(URI uri){
+        System.out.printf("""
+                -----------------------------------------
+                [scheme:]scheme-specific-part[#fragment]
+                ------------------------------------------
+                Scheme: %s
+                Scheme-specific part: %s
+                    Authority: %s
+                    User Info: %s
+                    Host: %s,
+                    Port: %s
+                    Path: %s
+                    Query:%s
+                Fragment: %s
+           """,
+                uri.getScheme(),
+                uri.getSchemeSpecificPart(),
+                uri.getAuthority(),
+                uri.getUserInfo(),
+                uri.getHost(),
+                uri.getPort(),
+                uri.getPath(),
+                uri.getQuery(),
+                uri.getFragment());
+    }
+
+
+    private static void  print(URL url){
+        System.out.printf("""
+           -----------------------------------------
+           
+            Authority: %s
+                User Info: %s
+                Host: %s,
+                Port: %s
+                Path: %s
+                Query:%s
+          
+           """,
+
+                url.getAuthority(),
+                url.getUserInfo(),
+                url.getHost(),
+                url.getPort(),
+                url.getPath(),
+                url.getQuery());
+
+    }
 }
--- HelloWorld/src/networking/WebContent.java
+++ HelloWorld/src/networking/WebContent.java
@@ -1,4 +1,42 @@
 package networking;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class WebContent {
+    public static void main(String[] args) {
+        try {
+          //  URL url = new URL(("http://example.com"));
+            URL url = new URL("http://jsonplaceholder.typicode.com/todos?id=6");
+
+           // printContents(url.openStream());
+
+            URLConnection urlConnection = url.openConnection();
+            System.out.println(urlConnection.getContentType());
+            urlConnection.getHeaderFields().entrySet().forEach(System.out::println);
+            System.out.println(urlConnection.getHeaderField("Cache-Control"));
+            urlConnection.connect();
+            System.out.println(urlConnection.getInputStream());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void printContents(InputStream is){
+        try(BufferedReader inputStream = new BufferedReader(new InputStreamReader(is))){
+            String line;
+            while ((line = inputStream.readLine() ) != null){
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
